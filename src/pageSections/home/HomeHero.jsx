bd19e44d6a9f3f89afef7e4a34135b48cc92853a
boggsey <eric@mlemedia.com>
2021-06-01 01:40:38 -0400

    Adding css files

--- src/pageSections/home/HomeHero.jsx
+++ src/pageSections/home/HomeHero.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function HomeHero() {
   return (
-    <div className="homepage-mission">
-      <p className="heading">Our Mission</p>
-      <p className="mission-message">A Voice for the Innocent is a non-profit community dedicated to the support of those affected by rape and sexual abuse by offering a safe and compassionate space to share stories and seek help.</p>
-      <a href="/tell-story/" className="btn-primary">Tell Your Story</a> <a href="/about/" className="about-us btn">Learn About AVFTI</a>
+    <div className="hero-mission">
+      <div className="hero-container">
+        <p className="hero-description">A Voice for the Innocent is a non-profit community dedicated to the support of those affected by rape and sexual abuse by offering a safe and compassionate space to share stories and seek help.</p>
+        <a href="/tell-story/" className="btn-primary mb-4 mr-2">Tell Your Story</a>
+        <a href="/about/" className="btn-ghost">Learn About AVFTI</a>
+      </div>
     </div>
   );
 }
